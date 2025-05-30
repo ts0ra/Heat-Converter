b2cbefc920a83497782215722c71913231deb6f4
ts0ra <gaming.heroes354@gmail.com>
2025-05-30 21:57:13 +0700

    feat: added convertion heat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,29 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import HeatInput from "../lib/components/HeatInput.svelte";
+  import Title from "../lib/components/Title.svelte";
+
+  let celcius: number = $state(0);
+  let fahrenheit: number = $state(0);
+  let kelvin: number = $state(0);
+
+  $effect(() => {
+    fahrenheit = parseFloat((((celcius * 9) / 5) + 32).toFixed(2));
+    kelvin = parseFloat((celcius + 273.15).toFixed(2));
+  });
+  $effect(() => {
+    celcius = parseFloat((((fahrenheit - 32) * 5) / 9).toFixed(2));
+    kelvin = parseFloat((celcius + 273.15).toFixed(2));
+  });
+  $effect(() => {
+    celcius = parseFloat((kelvin - 273.15).toFixed(2));
+    fahrenheit = parseFloat((((celcius * 9) / 5) + 32).toFixed(2));
+  });
+
+</script>
+
+<div class="container bg-amber-200 flex flex-col items-center justify-center min-h-screen">
+  <Title />
+  <HeatInput name="Celsius" bind:value={celcius} />
+  <HeatInput name="Fahrenheit" bind:value={fahrenheit} />
+  <HeatInput name="Kelvin" bind:value={kelvin} />
+</div>
