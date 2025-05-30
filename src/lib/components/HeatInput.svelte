23a20583471d42acbc00feb952d965d206184f69
ts0ra <gaming.heroes354@gmail.com>
2025-05-30 21:56:49 +0700

    feat: add HeatInput and Title components

---
+++ src/lib/components/HeatInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let { name, value = $bindable() } = $props();
+</script>
+
+<div class="flex max-w-84 w-screen h-14  justify-between items-center">
+  <p class="text-lg font-semibold">{name}</p>
+  <input
+    type="number"
+    bind:value
+    placeholder={`Enter ${name}`}
+    class="border border-gray-300 rounded"
+  />
+</div>
---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-3xl font-bold mb-2">Heat Converter</h1>
