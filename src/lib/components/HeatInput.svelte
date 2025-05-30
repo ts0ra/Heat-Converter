68f5ad9bdf0659f0abe1c1731eba96d620d71ee8
ts0ra <gaming.heroes354@gmail.com>
2025-05-30 22:01:54 +0700

    feat: add callback for tracking

--- src/lib/components/HeatInput.svelte
+++ src/lib/components/HeatInput.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-  let { name, value = $bindable() } = $props();
+  let { name, value = $bindable(), onInputChange } = $props();
+
+  function handleInput() {
+    if (onInputChange) {
+      onInputChange(name.toLowerCase());
+    }
+  }
 </script>
 
 <div class="flex max-w-84 w-screen h-14  justify-between items-center">
-  <p class="text-lg font-semibold">{name}</p>
-  <input
+  <p class="text-lg font-semibold">{name}</p>  <input
     type="number"
     bind:value
+    oninput={handleInput}
     placeholder={`Enter ${name}`}
     class="border border-gray-300 rounded"
   />
